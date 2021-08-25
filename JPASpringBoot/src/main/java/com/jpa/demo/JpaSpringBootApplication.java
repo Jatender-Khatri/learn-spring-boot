139eb5b5c6a543715ddb8a5fd0072e8ce2c1669e
Your NameJatender <kumarjatender0@gmail.com>
2021-08-25 16:53:25 +0500

    Curd In Spring Boot

--- JPASpringBoot/src/main/java/com/jpa/demo/JpaSpringBootApplication.java
+++ JPASpringBoot/src/main/java/com/jpa/demo/JpaSpringBootApplication.java
@@ -19,26 +19,21 @@ public class JpaSpringBootApplication {
 
 		StudentRepository repository = context.getBean(StudentRepository.class);
 		/*
-		 * Save One Record In the Database 
-		 * Student s1 = new Student();
+		 * Save One Record In the Database Student s1 = new Student();
 		 * s1.setAddress("Umerkot"); s1.setName("Jai Kumar");
 		 * s1.setRollNumber("2K19/AA/23"); Student s = repository.save(s1);
 		 * System.out.println("Student : " + s);
 		 */
 		/*
-		 * Save More than One Record In the Database 
-		 * List<Student> students =
-		 * List.of(s1,s2);
-		 * 
-		 * Iterable<Student> saveAll = repository.saveAll(students);
+		 * Save More than One Record In the Database List<Student> students =
+		 * List.of(s1,s2); Iterable<Student> saveAll = repository.saveAll(students);
 		 * 
 		 * saveAll.forEach(student->{ System.out.println(student); });
 		 * System.out.println("Successfully Done"); //Student s1 = repository.save(s);
 		 * //System.out.println("Student : " + s1);
 		 */
 		/*
-		 * Update The Student Record 
-		 * Optional<Student> optional =
+		 * Update The Student Record Optional<Student> optional =
 		 * repository.findById(2); Student s = optional.get();
 		 * s.setName("Naresh Kumar");
 		 * 
@@ -46,8 +41,7 @@ public class JpaSpringBootApplication {
 		 * System.out.println("Updated Successfully");
 		 */
 		/*
-		 * Get All Record From Database Method1 
-		 * Iterable<Student> getAll =
+		 * Get All Record From Database Method1 Iterable<Student> getAll =
 		 * repository.findAll();
 		 * 
 		 * Iterator<Student> iterator = getAll.iterator();
@@ -56,19 +50,16 @@ public class JpaSpringBootApplication {
 		 * System.out.println(s); }
 		 */
 		/*
-		 * Get All Record From Database Method2 
-		 * Iterable<Student> iterable =
+		 * Get All Record From Database Method2 Iterable<Student> iterable =
 		 * repository.findAll(); iterable.forEach(user->{System.out.println(user);});
 		 */
 		/*
-		 * Delete the Record By Id 
-		 * repository.deleteById(2);
+		 * Delete the Record By Id repository.deleteById(2);
 		 * System.out.println("Deleted Successfully");
 		 */
 
 		/*
-		 * Delete All The Record From Database 
-		 * Iterable<Student> iterable =
+		 * Delete All The Record From Database Iterable<Student> iterable =
 		 * repository.findAll();
 		 * 
 		 * iterable.forEach(student->{System.out.println(student);});
@@ -77,6 +68,52 @@ public class JpaSpringBootApplication {
 		 * 
 		 * System.out.println("Successfully Delete The All Record From Database");
 		 */
+		/*
+		 * Find By Name from Database List<Student> list =
+		 * repository.findByName("Jatender");
+		 * 
+		 * list.forEach(student->{System.out.println(student);});
+		 */
+		/*
+		 * Find By Roll Number from Database List<Student> list =
+		 * repository.findByRollNumber("18CS48");
+		 * list.forEach(student->{System.out.println(student);});
+		 * System.out.println("Data get find by Roll Number successfully");
+		 */
+		/*
+		 * Find By Address from Database List<Student> students =
+		 * repository.findByAddress("Umerkot");
+		 * students.forEach(student->{System.out.println(student);});
+		 * System.out.println("Data get find by Address successfully");
+		 */
+		List<Student> list = repository.findByNameAndRollNumber("Jatender", "18CS48");
+
+		list.forEach(student -> {
+			System.out.println(student);
+		});
+
+		System.out.println("Get Successfully");
+
+		List<Student> students = repository.getAllStudent();
+		students.forEach(student -> {
+			System.out.println(student);
+		});
+		System.out.println("Successfully Get All The Record From Database");
+		System.out.println("----------------------------------------");
+
+		List<Student> s = repository.getStudentByName("Jatender");
+		s.forEach(student -> {
+			System.out.println(student);
+		});
+		System.out.println("----------------------------------------");
+		List<Student> s1 = repository.getStudentByNameAndRollNumber("Raja", "18CS44");
+		s1.forEach(student -> {
+			System.out.println(student);
+		});
+		
+		System.out.println("----------------------------------------");
+		repository.getStudents().forEach(student->{System.out.println(student);});
+
 	}
 
 }
--- JPASpringBoot/src/main/java/com/jpa/demo/dao/StudentRepository.java
+++ JPASpringBoot/src/main/java/com/jpa/demo/dao/StudentRepository.java
@@ -1,9 +1,31 @@
 package com.jpa.demo.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.jpa.demo.entity.Student;
 
-public interface StudentRepository extends CrudRepository<Student, Integer>{
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+	public List<Student> findByName(String name);
 
+	public List<Student> findByRollNumber(String rollNumber);
+
+	public List<Student> findByAddress(String address);
+
+	public List<Student> findByNameAndRollNumber(String name, String rollNumber);
+
+	@Query("select s from Student s")
+	public List<Student> getAllStudent();
+
+	@Query("select s from Student s where s.name =:n")
+	public List<Student> getStudentByName(@Param("n") String name);
+
+	@Query("select s from Student s where s.name =:n and s.rollNumber =:r")
+	public List<Student> getStudentByNameAndRollNumber(@Param("n") String name, @Param("r") String rollNumber);
+
+	@Query(value = "select * from student", nativeQuery = true)
+	public List<Student> getStudents();
 }
